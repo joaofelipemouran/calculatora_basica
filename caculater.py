88c057a815572df1428dcda227809f0a1804d890
Joao Felipe <joaofelipenmoura@gmail.com>
2023-08-23 14:21:08 -0300

    adicionado a operção expoente

--- caculater.py
+++ caculater.py
@@ -1,16 +1,18 @@
-operacao = input("Digite a operacao desejada (soma, sub, mult, div): ")
-numero1 = input("Digite o primeiro número: ")
-numero2 = input("Digite o segundo número: ")
+operacao = input("Digite a operacao desejada (soma, sub, mult, div, exp): ")
+numero1 = float(input("Digite o primeiro número: "))  # Use float para permitir números decimais
+numero2 = float(input("Digite o segundo número: "))  # Use float para permitir números decimais
 
 if operacao == "soma":
-	resultado = int(numero1) + int(numero2)
-if operacao == "sub":
-	resultado = int(numero1) - int(numero2)
-if operacao == "mult":
-	resultado = int(numero1) * int(numero2)
-if operacao == "div":
-	resultado = int(numero1) / int(numero2)
+    resultado = numero1 + numero2
+elif operacao == "sub":
+    resultado = numero1 - numero2
+elif operacao == "mult":
+    resultado = numero1 * numero2
+elif operacao == "div":
+    resultado = numero1 / numero2
+elif operacao == "exp":
+    resultado = numero1 ** numero2
 else:
-	resultado = "Operação não suportada"
+    resultado = "Operação não suportada"
 
-print("O resultado da operação é: " + str(resultado))
+print("O resultado da operação é:", resultado)
